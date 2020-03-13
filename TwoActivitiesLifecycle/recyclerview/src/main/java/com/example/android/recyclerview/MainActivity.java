67083a975a63cc0c7f718e3549a537228958050c
Pedro Varela <pedrovarela86@users.noreply.github.com>
2020-03-13 19:39:20 -0400

    Migrated to android x

--- TwoActivitiesLifecycle/recyclerview/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ TwoActivitiesLifecycle/recyclerview/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -39,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
     private final LinkedList<String> mWordList = new LinkedList<>();
 
+
     private RecyclerView mRecyclerView;
-    private WordListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         // Create recycler view.
         mRecyclerView = findViewById(R.id.recyclerview);
         // Create an adapter and supply the data to be displayed.
-        mAdapter = new WordListAdapter(this, mWordList);
+        WordListAdapter mAdapter = new WordListAdapter(this, mWordList);
         // Connect the adapter with the recycler view.
         mRecyclerView.setAdapter(mAdapter);
         // Give the recycler view a default layout manager.
